8ac04c35f86fa392df77f1b073406fa86355d41b
vladislausbozh <113725514+vladislausbozh@users.noreply.github.com>
2023-05-17 14:57:08 +0300

    remove

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <img width={40} height={40} src="/img/logo.png" />
+          <div className="headerInfo">
+            <h3>Tea Hous</h3>
+            <p>Чай, оптом и в розницу</p>
+          </div>
+        </div>
+        <div className="headerRight">
+          <img src="/img/cart.svg" />
+          <span>Корзина</span>
+        </div>
       </header>
+      <div className="content">
+        <h1>Tea</h1>
+
+        <div className="cards">
+          <div className="card">
+            <div className="cardImg">
+              <img width={80} src="/img/icon-tea.png" alt="Tea" />
+            </div>
+            <p>Фруктовый чай - Бора Бора</p>
+            <div className="cardButton">
+              <b>150 руб</b>
+              <button className="button">+</button>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="cardImg">
+              <img width={80} src="/img/icon-tea.png" alt="Tea" />
+            </div>
+            <p>Фруктовый чай - Бора Бора</p>
+            <div className="cardButton">
+              <b>150 руб</b>
+              <button className="button">+</button>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="cardImg">
+              <img width={80} src="/img/icon-tea.png" alt="Tea" />
+            </div>
+            <p>Фруктовый чай - Бора Бора</p>
+            <div className="cardButton">
+              <b>150 руб</b>
+              <button className="button">+</button>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="cardImg">
+              <img width={80} src="/img/icon-tea.png" alt="Tea" />
+            </div>
+            <p>Фруктовый чай - Бора Бора</p>
+            <div className="cardButton">
+              <b>150 руб</b>
+              <button className="button">+</button>
+            </div>
+          </div>
+          
+        </div>
+
+      </div>
     </div>
   );
 }
