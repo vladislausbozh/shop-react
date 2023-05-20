f69c9cd2b43f7a6f9c2a2e88255b5a9cfead0e51
vladislausbozh <113725514+vladislausbozh@users.noreply.github.com>
2023-05-20 20:28:45 +0300

    component Drawer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Drawer from "./components/Drawer";
 
 
 function App() {
@@ -6,49 +7,7 @@ function App() {
     <div className="wrapper">
 
       <div className="overlay">
-        <div className="drawer">
-          <h3>Корзина</h3>
-          <div className="items">
-          <div className="cartItem">
-            <img width={70} height={70} src="./img/icon-tea.png" alt="Tea" />
-            <div>
-              <p>Фруктовый чай - Бора Бора</p>
-              <b>150 руб</b>
-            </div>
-            <img className="removeBtn"  src="/img/btn-close.svg" alt="Remove" />
-          </div>
-
-          <div className="cartItem">
-            <img width={70} height={70} src="./img/icon-tea.png" alt="Tea" />
-            <div>
-              <p>Фруктовый чай - Бора Бора</p>
-              <b>150 руб</b>
-            </div>
-            <img className="removeBtn"  src="/img/btn-close.svg" alt="Remove" />
-          </div>
-          </div>
-          <div className="cartTotal">
-            <ul>
-              <li>
-                <span>Цена без скидки</span>
-                <div></div>
-                <b>300 руб.</b>
-              </li>
-              <li>
-                <span>Скидка 5%</span>
-                <div></div>
-                <b>15 руб</b>
-              </li>
-              <li>
-                <span>Итого</span>
-                <div></div>
-                <b>285,00 руб</b>
-              </li>
-              
-            </ul>
-            <button>Заказать</button>
-          </div>
-        </div>
+        <Drawer />
       </div>
       <Header />
       
